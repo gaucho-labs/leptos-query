446de73c5448c74ff29c5a5fe02a68c1323e05e7
SleeplessOne1917 <insomnia-void@protonmail.com>
2024-01-27 00:14:13 -0500

    Upgrade leptos dependency to 0.6

--- example/start-axum/src/fileserv.rs
+++ example/start-axum/src/fileserv.rs
@@ -2,14 +2,13 @@ use cfg_if::cfg_if;
 
 cfg_if! { if #[cfg(feature = "ssr")] {
     use axum::{
-        body::{boxed, Body, BoxBody},
+        body::Body,
         extract::State,
-        response::IntoResponse,
+        response::{IntoResponse, Response as AxumResponse},
         http::{Request, Response, StatusCode, Uri},
     };
-    use axum::response::Response as AxumResponse;
     use tower::ServiceExt;
-    use tower_http::services::ServeDir;
+    use tower_http::services::{ServeDir, fs::ServeFileSystemResponseBody};
     use leptos::*;
     use crate::app::App;
 
@@ -20,17 +19,17 @@ cfg_if! { if #[cfg(feature = "ssr")] {
         if res.status() == StatusCode::OK {
             res.into_response()
         } else {
-            let handler = leptos_axum::render_app_to_stream(options.to_owned(), move ||  view!{ <App/>});
+            let handler = leptos_axum::render_app_to_stream(options.to_owned(), App);
             handler(req).await.into_response()
         }
     }
 
-    async fn get_static_file(uri: Uri, root: &str) -> Result<Response<BoxBody>, (StatusCode, String)> {
+    async fn get_static_file(uri: Uri, root: &str) -> Result<Response<ServeFileSystemResponseBody>, (StatusCode, String)> {
         let req = Request::builder().uri(uri.clone()).body(Body::empty()).unwrap();
         // `ServeDir` implements `tower::Service` so we can call it with `tower::ServiceExt::oneshot`
         // This path is relative to the cargo root
         match ServeDir::new(root).oneshot(req).await {
-            Ok(res) => Ok(res.map(boxed)),
+            Ok(res) => Ok(res),
             Err(err) => Err((
                 StatusCode::INTERNAL_SERVER_ERROR,
                 format!("Something went wrong: {err}"),
