424cf9ea1f34aa1ab3cdb866ac3dc8eec9b7787b
Nico Burniske <nicoburniske@gmail.com>
2024-02-19 15:44:30 -0500

    remove cfg_if in favor of module cfg

--- example/start-axum/src/fileserv.rs
+++ example/start-axum/src/fileserv.rs
@@ -1,35 +1,44 @@
-use cfg_if::cfg_if;
+use crate::app::App;
+use axum::{
+    body::Body,
+    extract::State,
+    http::{Request, Response, StatusCode, Uri},
+    response::{IntoResponse, Response as AxumResponse},
+};
+use leptos::*;
+use tower::ServiceExt;
+use tower_http::services::{fs::ServeFileSystemResponseBody, ServeDir};
 
-cfg_if! { if #[cfg(feature = "ssr")] {
-    use axum::{
-        body::Body,
-        extract::State,
-        response::{IntoResponse, Response as AxumResponse},
-        http::{Request, Response, StatusCode, Uri},
-    };
-    use tower::ServiceExt;
-    use tower_http::services::{ServeDir, fs::ServeFileSystemResponseBody};
-    use leptos::*;
-    use crate::app::App;
+pub async fn file_and_error_handler(
+    uri: Uri,
+    State(options): State<LeptosOptions>,
+    req: Request<Body>,
+) -> AxumResponse {
+    let root = options.site_root.clone();
+    let res = get_static_file(uri.clone(), &root).await.unwrap();
 
-    pub async fn file_and_error_handler(uri: Uri, State(options): State<LeptosOptions>, req: Request<Body>) -> AxumResponse {
-        let root = options.site_root.clone();
-        let res = get_static_file(uri.clone(), &root).await.unwrap();
-
-        if res.status() == StatusCode::OK {
-            res.into_response()
-        } else {
-            let handler = leptos_axum::render_app_to_stream(options.to_owned(), App);
-            handler(req).await.into_response()
-        }
+    if res.status() == StatusCode::OK {
+        res.into_response()
+    } else {
+        let handler = leptos_axum::render_app_to_stream(options.to_owned(), App);
+        handler(req).await.into_response()
     }
+}
 
-    async fn get_static_file(uri: Uri, root: &str) -> Result<Response<ServeFileSystemResponseBody>, (StatusCode, String)> {
-        let req = Request::builder().uri(uri.clone()).body(Body::empty()).unwrap();
-        // `ServeDir` implements `tower::Service` so we can call it with `tower::ServiceExt::oneshot`
-        // This path is relative to the cargo root
-        ServeDir::new(root)
-            .oneshot(req).await
-            .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, format!("Something went wrong: {err}")))
-    }
-}}
+async fn get_static_file(
+    uri: Uri,
+    root: &str,
+) -> Result<Response<ServeFileSystemResponseBody>, (StatusCode, String)> {
+    let req = Request::builder()
+        .uri(uri.clone())
+        .body(Body::empty())
+        .unwrap();
+    // `ServeDir` implements `tower::Service` so we can call it with `tower::ServiceExt::oneshot`
+    // This path is relative to the cargo root
+    ServeDir::new(root).oneshot(req).await.map_err(|err| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {err}"),
+        )
+    })
+}
--- example/start-axum/src/lib.rs
+++ example/start-axum/src/lib.rs
@@ -1,22 +1,14 @@
-use cfg_if::cfg_if;
 pub mod app;
 pub mod error_template;
+#[cfg(feature = "ssr")]
 pub mod fileserv;
 pub mod todo;
 
-cfg_if! { if #[cfg(feature = "hydrate")] {
-    use leptos::*;
-    use wasm_bindgen::prelude::wasm_bindgen;
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
     use crate::app::*;
-
-    #[wasm_bindgen]
-    pub fn hydrate() {
-        // initializes logging using the `log` crate
-        _ = console_log::init_with_level(log::Level::Debug);
-        console_error_panic_hook::set_once();
-
-        leptos::mount_to_body(move || {
-            view! {  <App/> }
-        });
-    }
-}}
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    leptos::mount_to_body(App);
+}
