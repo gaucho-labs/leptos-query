a0949ec26b3baa0c9b3c3055fef6170812025529
Nico Burniske <nicoburniske@gmail.com>
2024-02-20 11:30:03 -0500

    breakup persister with feature flags

--- query/src/query_persister/local_storage.rs
+++ query/src/query_persister/local_storage.rs
@@ -1,5 +1,4 @@
 use crate::query_persister::*;
-use cfg_if::cfg_if;
 
 /// A persister that uses local storage to persist queries.
 #[derive(Clone, Copy)]
@@ -15,62 +14,53 @@ thread_local! {
 fn local_storage() -> Option<web_sys::Storage> {
     LOCAL_STORAGE.with(Clone::clone)
 }
-
+#[cfg(any(feature = "hydrate", feature = "csr"))]
 #[async_trait(?Send)]
 impl QueryPersister for LocalStoragePersister {
     async fn persist(&self, key: &str, query: PersistQueryData) {
-        cfg_if! {
-            if #[cfg(any(feature = "hydrate", feature = "csr"))] {
-                if let Some(storage) = local_storage() {
-                    let value = miniserde::json::to_string(&query);
-                    let _ = storage.set(&key, &value);
-                }
-            } else {
-                let _ = query;
-                let _ = key;
-                ()
-            }
+        if let Some(storage) = local_storage() {
+            let value = miniserde::json::to_string(&query);
+            let _ = storage.set(&key, &value);
         }
     }
 
     async fn remove(&self, key: &str) {
-        cfg_if! {
-            if #[cfg(any(feature = "hydrate", feature = "csr"))] {
-                if let Some(storage) = local_storage() {
-                    let _ = storage.remove_item(key);
-                }
-            } else {
-                let _ = key;
-                ()
-            }
+        if let Some(storage) = local_storage() {
+            let _ = storage.remove_item(key);
         }
     }
 
     async fn retrieve(&self, key: &str) -> Option<PersistQueryData> {
-        cfg_if! {
-            if #[cfg(any(feature = "hydrate", feature = "csr"))] {
-                if let Some(storage) = local_storage() {
-                    if let Some(value) = storage.get_item(key).ok().flatten() {
-                        return miniserde::json::from_str(&value).ok()
-                    }
-                }
-                None
-            } else {
-                let _ = key;
-                None
+        if let Some(storage) = local_storage() {
+            if let Some(value) = storage.get_item(key).ok().flatten() {
+                return miniserde::json::from_str(&value).ok();
             }
         }
+        None
     }
 
     async fn clear(&self) {
-        cfg_if! {
-            if #[cfg(any(feature = "hydrate", feature = "csr"))] {
-                if let Some(storage) = local_storage() {
-                    let _ = storage.clear();
-                }
-            } else {
-                ()
-            }
+        if let Some(storage) = local_storage() {
+            let _ = storage.clear();
         }
     }
+}
+
+#[cfg(not(any(feature = "hydrate", feature = "csr")))]
+impl QueryPersister for LocalStoragePersister {
+    async fn persist(&self, key: &str, query: PersistQueryData) {
+        let _ = key;
+        let _ = query;
+    }
+
+    async fn remove(&self, key: &str) {
+        let _ = key;
+    }
+
+    async fn retrieve(&self, key: &str) -> Option<PersistQueryData> {
+        let _ = key;
+        None
+    }
+
+    async fn clear(&self) {}
 }
